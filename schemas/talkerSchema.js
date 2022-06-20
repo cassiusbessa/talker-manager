b0944f6576c56fdeb067359deda3dff7b3e9acfc
cassiusbessa <cassiusbessa@gmail.com>
2022-06-20 18:50:44 -0300

    Schema do palestrante feito em talkerSchema

---
+++ schemas/talkerSchema.js
@@ -0,0 +1,30 @@
+const Joi = require('joi').extend(require('@joi/date'));
+
+const talkerSchema = Joi.object({
+  name: Joi.string().required().min(3)
+    .messages({
+      'string.min': 'O "name" deve ter pelo menos 3 caracteres',
+      'any.required': 'O campo "name" é obrigatório',
+   }),
+  age: Joi.number().required().min(18)
+    .messages({
+      'any.required': 'O campo "age" é obrigatório',
+      'string.min': 'A pessoa palestrante deve ser maior de idade',
+ }),
+ talk: Joi.object({
+  watchedAt: Joi.date().format('DD/MM/YYYY').required()
+  .messages({
+    'any.required': 'O campo "watchedAt" é obrigatório',
+    'date.format': 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"',
+  }),
+  rate: Joi.number().required().min(1).max(5)
+  .messages({
+    'any.required': 'O campo "rate" é obrigatório',
+    'string.min': 'O campo "rate" deve ser um inteiro de 1 à 5',
+    'string.max': 'O campo "rate" deve ser um inteiro de 1 à 5',
+
+  }),
+ }).required(),
+}).required();
+
+module.exports = talkerSchema;
